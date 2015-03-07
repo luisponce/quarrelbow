74d7837d0ab45ef66276c69b540fed9aeb65d4dd
luis M Ponce <luisponcekun@gmail.com>
2015-03-07 07:23:04 -0500

    Fix #1 - Character movement

--- Quarrel Bow Unity/Assets/Standard Assets/2D/Scripts/CameraFollow.cs
+++ Quarrel Bow Unity/Assets/Standard Assets/2D/Scripts/CameraFollow.cs
@@ -6,7 +6,9 @@ namespace UnityStandardAssets._2D
 {
     public class CameraFollow : MonoBehaviour
     {
-        public float xMargin = 1f; // Distance in the x axis the player can move before the camera follows.
+
+
+		public float xMargin = 1f; // Distance in the x axis the player can move before the camera follows.
         public float yMargin = 1f; // Distance in the y axis the player can move before the camera follows.
         public float xSmooth = 8f; // How smoothly the camera catches up with it's target movement in the x axis.
         public float ySmooth = 8f; // How smoothly the camera catches up with it's target movement in the y axis.
