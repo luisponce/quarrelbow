96b38cf02f82e4fbc938fb732d7e052ef6b9fb2b
luis Ponce <luisponcekun@gmail.com>
2015-03-07 11:52:05 -0500

    Fix #2 - Arrow firing

--- Quarrel Bow Unity/Assets/Quarrel Bow/ArrowFiring.cs
+++ Quarrel Bow Unity/Assets/Quarrel Bow/ArrowFiring.cs
@@ -7,7 +7,11 @@ public class ArrowFiring : MonoBehaviour {
 	public GameObject arrowPrefab;
 	public Vector2 characterSize = new Vector2(0.4f, 3f);
 
+	public float timeToReachMaxForce = 2;
+	public float maxForce = 1000;
+
 	private Vector3 dir;
+	private float timerFiring = 0; //time between the press of the button and the release and shoot of the arrow
 
 
 	// Use this for initialization
@@ -23,9 +27,19 @@ public class ArrowFiring : MonoBehaviour {
 
 
 		Debug.DrawLine(transform.position, mousePosition, Color.blue);
-		if(Input.GetKeyUp(KeyCode.Mouse0)){
-			ShootArrow(dir, 1000);
 
+		if(Input.GetKeyUp(KeyCode.Mouse0)){
+			ShootArrow(dir, maxForce * timerFiring/timeToReachMaxForce);
+
+			timerFiring = 0;
+		} else {
+			if(Input.GetKey(KeyCode.Mouse0)){
+				if(timerFiring < timeToReachMaxForce){
+					timerFiring += Time.deltaTime;
+				} else {
+					timerFiring = timeToReachMaxForce;
+				}
+			}
 		}
 	}
 
@@ -37,7 +51,7 @@ public class ArrowFiring : MonoBehaviour {
 		Quaternion r = transform.rotation;
 		r.SetLookRotation(Vector3.forward, -Vector3.Cross(dir,Vector3.forward));
 
-		Debug.DrawRay(pos, dir*force, Color.red, 5f);
+		Debug.DrawRay(pos, dir*(force/maxForce)*10, Color.red, 5f);
 
 		GameObject cur =  (GameObject) Transform.Instantiate((Object) arrowPrefab, pos, r);
 		cur.GetComponent<Rigidbody2D>().AddForce(cur.transform.right * force);
