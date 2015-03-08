ed0e3c5856ae85a9746195162a3cadbb4411766c
Rodrigo Diaz <pericodiaz89@gmail.com>
2015-03-08 18:25:44 -0500

    Android animation tests with Animator and blend tree, also Shocky's AI implementation.

---
+++ Quarrel Bow Unity/Assets/Quarrel Bow/Elements/Enemies/Shoky/Shoky.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoky : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + Vector3.forward * -90f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + Vector3.forward * -90f);
+        GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+    }
+}
