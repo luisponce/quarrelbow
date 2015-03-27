82728fb3af2a06e1b61f5d15b9f8d4607785532a
Rodrigo Diaz <pericodiaz89@gmail.com>
2015-03-27 11:56:34 -0500

    Arrow fix and shocky killed

--- Quarrel Bow Unity/Assets/Quarrel Bow/Elements/Arrow/Arrow.cs
+++ Quarrel Bow Unity/Assets/Quarrel Bow/Elements/Arrow/Arrow.cs
@@ -33,16 +33,17 @@ public class Arrow : MonoBehaviour {
         }
         else if (collision.gameObject.layer == (int)ELayer.Enemy)
         {
-            transform.Translate(transform.forward * 0.5f);
+            transform.Translate(transform.forward * 1.5f);
             transform.SetParent(collision.transform);
             collision.transform.GetComponent<Shoky>().Kill();
             collision.transform.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
-            Destroy(GetComponent<Rigidbody>());
+            Destroy(GetComponent<Rigidbody2D>());
             Destroy(GetComponent<BoxCollider2D>());
             State = EArrowState.Ignored;
         }
         else
         {
+            Debug.Log(((ELayer)collision.gameObject.layer) + ", " + collision.gameObject.name);
             State = EArrowState.Ignored;
             Destroy(gameObject, 10f);
         }
@@ -65,6 +66,7 @@ public class Arrow : MonoBehaviour {
         set
         {
             state = value;
+            //Debug.Log(state);
             if (value == EArrowState.Ignored)
             {
                 gameObject.layer = (int)ELayer.Ignore;
